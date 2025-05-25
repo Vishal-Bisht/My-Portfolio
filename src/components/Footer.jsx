cb8e03405dfa2e60ba46fde6a4a982d18cfe0a59
Vishal-Bisht <bishtvishal390@gmail.com>
2025-05-25 18:35:06 +0530

    LInks added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,56 @@
-import { Instagram, Linkedin, Twitter, Github } from "lucide-react";
+import { Linkedin } from "lucide-react";
 
 const Footer = () => (
   <footer className="mt-10 text-center text-gray-400">
     <h2 className="text-lg font-semibold mb-2">Connect with me</h2>
     <div className="flex justify-center gap-6 mt-2">
       <a
-        href="https://instagram.com/yourusername"
+        href="https://www.instagram.com/vishal_bisht001/"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Instagram"
       >
-        <Instagram className="h-6 w-6 hover:text-pink-500 transition" />
+        <img
+          src="/icons/instagram.svg"
+          alt="Instagram"
+          className="h-6 w-6 hover:brightness-125 transition"
+        />
       </a>
       <a
-        href="https://linkedin.com/in/yourusername"
+        href="https://www.linkedin.com/in/vishal-bist-4a5238261/"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="LinkedIn"
       >
-        <Linkedin className="h-6 w-6 hover:text-blue-500 transition" />
+         <img
+          src="/icons/linkedin.svg"
+          alt="Twitter"
+          className="h-6 w-6 hover:brightness-125 transition"
+        />
       </a>
       <a
-        href="https://twitter.com/yourusername"
+        href="https://x.com/vishal_bisht128?t=H3wtKei4TBHEBUrorEmGXw&s=09"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Twitter"
       >
-        <Twitter className="h-6 w-6 hover:text-sky-400 transition" />
+        <img
+          src="/icons/x.svg"
+          alt="Twitter"
+          className="h-6 w-6 hover:brightness-125 transition"
+        />
       </a>
       <a
-        href="https://github.com/yourusername"
+        href="https://github.com/Vishal-Bisht"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="GitHub"
       >
-        <Github className="h-6 w-6 hover:text-gray-100 transition" />
+        <img
+          src="/icons/github.svg"
+          alt="GitHub"
+          className="h-6 w-6 hover:brightness-125 transition"
+        />
       </a>
     </div>
   </footer>
